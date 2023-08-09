9e30ccf57c37ea98ee019ccfbc583c34d3a2fbf5
NilBrains <167288520@qq.com>
2023-08-09 16:09:59 +0800

    feat: add msg

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,9 @@
 import "./css/main.css";
 
 export * as loading from "./comps/loading";
+
+console.log(
+  "%cNILBRAINS%cme@nilbrains.com",
+  "padding: 8px 8px 8px 8px;background: #23aaf2; color: #fff;border-radius: 6px 0 0 6px;",
+  "padding: 8px 8px 8px 8px;background: #80c342; color: #fff;border-radius: 0 6px 6px 0;"
+);
